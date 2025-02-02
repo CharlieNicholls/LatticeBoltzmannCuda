090f188814a0c8689d82ec089d496301de2ff153
Charlie Nicholls <charlie.nicholls885@gmail.com>
2025-02-02 18:24:11 +0000

    Add check for if point is inside closed model (#7)

--- simulation/include/Model.h
+++ simulation/include/Model.h
@@ -1,9 +1,9 @@
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/Polygon_mesh_processing/IO/polygon_mesh_io.h>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Simple_cartesian.h>
 #include <string>
 
-using Meshdata = CGAL::Surface_mesh<CGAL::Exact_predicates_inexact_constructions_kernel::Point_3>;
+using Meshdata = CGAL::Surface_mesh<CGAL::Simple_cartesian<double>::Point_3>;
 
 class Model
 {
@@ -12,8 +12,13 @@ public:
 
     bool isModelClosed();
 
+    bool isPointInsideModel(CGAL::Simple_cartesian<double>::Point_3 point);
+
     const Meshdata& getMesh() const { return modelMesh; };
+
+    bool checkError() { return !meshValid; };
 
 private:
     Meshdata modelMesh;
+    bool meshValid = true;
 };
